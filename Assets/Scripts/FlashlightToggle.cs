76f8671e0b7132fff25df2c14d2bd89401caa49f
David Brennan <davidwbrennan@hotmail.com>
2019-03-23 20:44:30 +0000

    Update .gitignore. Update room scene.

---
+++ Assets/Scripts/FlashlightToggle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using AuraAPI;
+using UnityEngine;
+
+public class FlashlightToggle : MonoBehaviour
+{
+    private AuraLight _auraLight;
+    private Light _light;
+
+    void Start()
+    {
+        _light = GetComponentInChildren<Light>();
+        _auraLight = GetComponentInChildren<AuraLight>();
+    }
+
+	void Update ()
+	{
+	    if (Input.GetKeyDown(KeyCode.F))
+	    {
+	        _light.enabled = !_light.enabled;
+            _auraLight.enabled = !_auraLight.enabled;
+        }	        
+	}
+}
